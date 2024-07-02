fdd2eeb996e7b9aa403338c6cce29a1cbfdb3304
Valyon450 <valentinbasuk@gmail.com>
2024-07-02 15:58:33 +0300

    feat(BusinessLogic): Add Automapper.

---
+++ OutOfOffice.Backend/BusinessLogic/Mappings/AutomapperProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using BusinessLogic.DTOs;
+using DataAccess.Entities;
+
+namespace BusinessLogic.Mappings
+{
+    public class AutomapperProfile : Profile
+    {
+        public AutomapperProfile()
+        {
+            CreateMap<Employee, EmployeeDTO>()
+                .ReverseMap();
+
+            CreateMap<LeaveRequest, LeaveRequestDTO>()
+                .ReverseMap();
+
+            CreateMap<ApprovalRequest, ApprovalRequestDTO>()
+                .ReverseMap();
+
+            CreateMap<Project, ProjectDTO>()
+                .ReverseMap();
+
+        }
+    }
+}
